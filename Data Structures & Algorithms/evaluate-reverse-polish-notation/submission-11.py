f69d4ed88a5404c70c34e201f02d78f176e8d258
ItsLame <37344470+ItsLame@users.noreply.github.com>
2026-09-13 17:21:13 +1000

    Add: evaluate-reverse-polish-notation - submission-11

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-11.py
@@ -0,0 +1,46 @@
+class Solution:
+    def plus(self, a: int, b: int) -> int:
+        return int(a + b)
+
+    def minus(self, a: int, b: int) -> int:
+        return int(a - b)
+
+    def multiply(self, a: int, b: int) -> int:
+        return int(a * b)
+    
+    def divide (self, a: int, b: int) -> int:
+        return int(a / b)
+
+    def isInt(self, val: str) -> bool:
+        try:
+            int(val)
+            return True
+        except:
+            return False
+    
+    def isInt(self, val: str) -> bool:
+        try:
+            int(val)
+            return True
+        except:
+            return False
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        nums = []
+
+        ops = {
+            "+": self.plus,
+            "-": self.minus,
+            "*": self.multiply,
+            "/": self.divide,
+        }
+
+        for t in tokens:
+            if self.isInt(t):
+                nums.append(int(t))
+            else:
+                func = ops.get(t)
+                nums[-2] = func(nums[-2], nums[-1])
+                nums.pop()
+
+        return nums[0]
