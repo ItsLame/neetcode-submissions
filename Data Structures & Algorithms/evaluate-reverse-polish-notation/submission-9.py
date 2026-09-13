5378ed6c3ee0bc7c35936faeabb9ce75af089f3e
ItsLame <37344470+ItsLame@users.noreply.github.com>
2026-09-13 17:20:04 +1000

    Add: evaluate-reverse-polish-notation - submission-9

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-9.py
@@ -0,0 +1,58 @@
+class Solution:
+    def plus(self, a: int, b: int) -> int:
+        return int(a + b)
+
+    def minus(self, a: int, b: int) -> int:
+        return int(a - b)
+
+    def multiply(self, a: int, b: int) -> int:
+        return int(a * b)
+    
+    def divide (self, a: int, b: int) -> int:
+        return int(a / b)
+
+    def isInt(self, val: str) -> bool:
+        try:
+            int(val)
+            return True
+        except:
+            return False
+    
+    def isInt(self, val: str) -> bool:
+        try:
+            int(val)
+            return True
+        except:
+            return False
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        nums = []
+
+        ops = {
+            "+": self.plus,
+            "-": self.minus,
+            "*": self.multiply,
+            "/": self.divide,
+        }
+
+        for t in tokens:
+            if self.isInt(t):
+                nums.append(int(t))
+            else:
+                func = ops.get(t)
+                func(nums[-2], nums[-1])
+                
+                if t == "+":
+                    nums[-2] = int(self.plus(nums[-2], nums[-1]))
+                    nums.pop()
+                elif t == "-":
+                    nums[-2] = int(self.minus(nums[-2], nums[-1]))
+                    nums.pop()
+                elif t == "*":
+                    nums[-2] = int(self.multiply(nums[-2], nums[-1]))
+                    nums.pop()
+                elif t == "/":
+                    nums[-2] = int(self.divide(nums[-2], nums[-1]))
+                    nums.pop()
+
+        return int(nums[0])
